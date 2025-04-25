3ebcf32e3cbd3ec8d6a49f3a0b00a097671ffff9
st 零 <435029375@qq.com>
2025-04-25 15:11:03 +0800

    action

---
+++ ediblegod-template-1.20.1/src/main/java/com/huangcy/ediblegod/EdibleGodClient.java
@@ -0,0 +1,10 @@
+package com.huangcy.ediblegod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EdibleGodClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
